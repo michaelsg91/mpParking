1e21116b82da980a17d7f7efdd325f1e27881ebd
Michael Saldarriaga <michael91sg@gmail.com>
2017-03-19 16:38:55 -0500

    create database on mysql

---
+++ src/vista/marcoPrincipal.java
@@ -0,0 +1,8 @@
+package vista;
+import javax.swing.*;
+
+public class marcoPrincipal extends JFrame{
+	public marcoPrincipal(){
+		
+	}
+}
---
+++ src/vista/mpParking.java
@@ -0,0 +1,9 @@
+package vista;
+
+public class mpParking {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
